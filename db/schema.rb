55ac5c12f1339849d0a61799c0599f179550e8c7
jpileto <jp.ileto@icloud.com>
2017-04-14 01:19:48 -0400

    Removinf widgets table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170414050426) do
+ActiveRecord::Schema.define(version: 20170414051836) do
 
   create_table "locations", force: :cascade do |t|
     t.string  "name"
@@ -24,18 +24,6 @@ ActiveRecord::Schema.define(version: 20170414050426) do
     t.string  "manufacturer"
     t.decimal "cost"
     t.decimal "quantity"
-  end
-
-  create_table "widgets", force: :cascade do |t|
-    t.string  "first_name"
-    t.string  "last_name"
-    t.string  "dietary_restrictions"
-    t.integer "salary"
-    t.integer "number_of_kids"
-    t.string  "vulnerabilities"
-    t.string  "illnesses"
-    t.string  "medication"
-    t.string  "voting_preferences"
   end
 
 end
